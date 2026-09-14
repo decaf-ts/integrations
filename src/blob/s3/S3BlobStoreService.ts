224c6d1e0b9fb5c8bfa3053727dc17fc616b5dda
decaf-ts-admin <decaf.ts.admin@gmail.com>
2026-09-14 14:32:14 +0100

    patch - DECAF-809 s3 injectablefix [skip ci] - after release preparation

--- src/blob/s3/S3BlobStoreService.ts
+++ src/blob/s3/S3BlobStoreService.ts
@@ -3,11 +3,16 @@
  * @summary AWS S3 blob store service.
  * @description Thin `@service`-decorated provider on top of {@link S3CompatibleBlobStoreService}.
  */
-import { service } from "@decaf-ts/core";
+import { Context, ContextualArgs, service } from "@decaf-ts/core";
 import { S3CompatibleBlobStoreService } from "./S3CompatibleBlobStoreService";
 import { S3BlobEnvironment } from "./S3BlobEnvironment";
-import { envBoolean, envNumber, envString } from "../../shared/environmentValue";
+import {
+  envBoolean,
+  envNumber,
+  envString,
+} from "../../shared/environmentValue";
 import type { S3BlobStoreServiceConfig } from "../core/BlobTypes";
+import { S3Client } from "@aws-sdk/client-s3";
 
 @service("blob-s3")
 export class S3BlobStoreService extends S3CompatibleBlobStoreService {
@@ -31,10 +36,21 @@ export class S3BlobStoreService extends S3CompatibleBlobStoreService {
       credentials: accessKeyId
         ? {
             accessKeyId,
-            secretAccessKey: envString(env.credentials?.secretAccessKey) as string,
+            secretAccessKey: envString(
+              env.credentials?.secretAccessKey
+            ) as string,
             sessionToken: envString(env.credentials?.sessionToken),
           }
         : undefined,
     };
+  }
+
+  override async initialize(
+    ...args: ContextualArgs<any>
+  ): Promise<{ config: S3BlobStoreServiceConfig; client: S3Client }> {
+    let cfg: any;
+    if (!args[0] || args[0] instanceof Context)
+      cfg = this.configFromEnvironment();
+    return super.initialize(cfg, ...args);
   }
 }
